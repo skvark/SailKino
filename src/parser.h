09aeb414a585233eb9f557d3285252b9a9bfdef6
Olli-Pekka Heinisuo <o-p@unknownpixels.com>
2014-09-27 22:46:25 +0300

    remove some includes

--- src/parser.h
+++ src/parser.h
@@ -4,8 +4,6 @@
 #include <QString>
 #include <QList>
 #include <httpEngine.h>
-#include <kitchen.h>
-#include <helperFunctions.h>
 #include <QVector>
 #include <QTextCodec>
 #include <QNetworkReply>
